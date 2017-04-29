d6a1234b0533141ad3127dc3b429145a0e90c27a
fortize <javivi42@hotmail.com>
2017-04-29 21:36:21 +0200

    Modificaciones para merge

--- Carga_archivos.R
+++ Carga_archivos.R
@@ -1,10 +1,15 @@
 #Load libraries
-library(csv)
+
 library(ggplot2)
 library(data.table)
 library(caret)
+library(kableExtra)
+library(xtable)
 
-macro <- read.csv(file="macro.csv", header=TRUE, sep=",")
-train <- read.csv2(file="train.csv", header=TRUE, sep=",")
-test <- read.csv2(file="test.csv", header=TRUE, sep=",")
+macro <- read.table(file="input/macro.csv", header=TRUE, sep=",")
+train <- read.table(file="input/train.csv", header=TRUE, sep=",")
+test <- read.table(file="input/test.csv", header=TRUE, sep=",")
 
+names(train)
+
+test_macro <- merge(test,macro,"timestamp")
